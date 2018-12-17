633a4c7bf01f11f6543e4d76ada33995ba151ca6
Alex Bochel <alexbochel@Alexs-Air.home>
2018-12-17 12:14:33 -0500

    Added custom cell type for main page

---
+++ Rest Easy/Views/AlarmTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AlarmTableViewCell.swift
+//  Rest Easy
+//
+//  Created by Alex Bochel on 12/17/18.
+//  Copyright © 2018 Sleepiez, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var AlarmTimeLabel: UILabel!
+    @IBOutlet weak var AlarmArmedSwitch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
