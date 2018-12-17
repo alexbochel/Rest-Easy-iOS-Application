e25399ec80156b297ac19554b82ad02e39f0b55c
Alex Bochel <alexbochel@Alexs-Air.home>
2018-12-17 11:01:25 -0500

    Added alarm model class

---
+++ Rest Easy/Models/AlarmModel.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmModel.swift
+//  Rest Easy
+//
+//  Created by Alex Bochel on 12/17/18.
+//  Copyright © 2018 Sleepiez, LLC. All rights reserved.
+//
+
+import Foundation
+
+class AlarmModel {
+    // Alarm time of day
+    // Alarm style type
+    // Alarm strength
+    // Alarm Weekdays
+}
